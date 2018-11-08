30a125f43bbc50a85fa6675171e53fd71a0b37d0
vikumkbv <vikumkbv@esportlk.com>
2018-11-09 00:31:58 +0530

    book adding form

---
+++ src/app/componet/item-book/item-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-book',
+  templateUrl: './item-book.component.html',
+  styleUrls: ['./item-book.component.css']
+})
+export class ItemBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
