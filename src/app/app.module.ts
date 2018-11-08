3f26fae5ad739dcbb396e94e9fd3fa28bc5c5a73
vikumkbv <vikumkbv@esportlk.com>
2018-11-08 14:10:18 +0530

    update add item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,24 @@ import { NavbarComponent } from './componet/navbar/navbar.component';
 import { FooterComponent } from './componet/footer/footer.component';
 import { FormAdditemComponent } from './componet/form-additem/form-additem.component';
 import { FormRemitemComponent } from './componet/form-remitem/form-remitem.component';
+import { ListitemsComponent } from './componet/form-listitems/listitems.component';
+import { BorrowitemComponent } from './componet/form-borrowitem/borrowitem.component';
+import { ReturnitemComponent } from './componet/item-returnitem/returnitem.component';
+import { ReportgenComponent } from './componet/form-reportgen/reportgen.component';
+import { FormReportgenComponent } from './componet/form-reportgen/form-reportgen.component';
+import { FormReturnitemComponent } from './componet/form-returnitem/form-returnitem.component';
+import { FormBorrowitemComponent } from './componet/form-borrowitem/form-borrowitem.component';
+import { FormListitemsComponent } from './componet/form-listitems/form-listitems.component';
+import { FormRemitemsComponent } from './componet/form-remitems/form-remitems.component';
+
+const appRoutes: Routes = [
+  { path: 'componet/form-additem', component: FormAdditemComponent },
+  { path: 'componet/form-borrowitem', component: FormBorrowitemComponent },
+  { path: 'componet/form-listitems', component: FormListitemsComponent },
+  { path: 'componet/form-remitems', component: FormRemitemsComponent },
+  { path: 'componet/form-reportgen', component: FormReportgenComponent },
+  { path: 'componet/form-returnitem', component: FormReturnitemComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,12 +32,23 @@ import { FormRemitemComponent } from './componet/form-remitem/form-remitem.compo
     NavbarComponent,
     FooterComponent,
     FormAdditemComponent,
-    FormRemitemComponent
+    FormBorrowitemComponent,
+    FormListitemsComponent,
+    FormRemitemsComponent,
+    FormReportgenComponent,
+    FormReturnitemComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+      ),
+      BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
---
+++ src/app/componet/form-additem/form-additem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-additem',
+  templateUrl: './form-additem.component.html',
+  styleUrls: ['./form-additem.component.css']
+})
+export class FormAdditemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
