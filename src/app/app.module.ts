c1df6c8248db49689be5a4607e057b18ab5b79d4
vikumkbv <vikumkbv@esportlk.com>
2018-11-08 12:11:03 +0530

    all upto now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componet/navbar/navbar.component';
+import { FooterComponent } from './componet/footer/footer.component';
+import { FormAdditemComponent } from './componet/form-additem/form-additem.component';
+import { FormRemitemComponent } from './componet/form-remitem/form-remitem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    FormAdditemComponent,
+    FormRemitemComponent
   ],
   imports: [
     BrowserModule
