8ce5c5677d23a40af91f10a13b930d4b1866ac2a
vikumkbv <vikumkbv@esportlk.com>
2018-11-09 00:39:31 +0530

    dvd insert

---
+++ src/app/componet/item-dvd/item-dvd.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-item-dvd',
+  templateUrl: './item-dvd.component.html',
+  styleUrls: ['./item-dvd.component.css']
+})
+export class ItemDvdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
